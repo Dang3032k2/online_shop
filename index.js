c78f1fa6d755b469681f2be377c1514c54b609ee
dangnguyen2k2 <nguyenthidang303@gmail.com>
2024-04-04 16:30:08 +0700

    fix js comment

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 let index = 0;
+const grid = document.getElementById("grid");
+const previousBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+const itemsPerPage =
+  window.innerWidth >= 1410 ? 3 : window.innerWidth >= 768 ? 2 : 1;
+const maxIndex = grid.children.length - itemsPerPage;
+const dots = document.querySelectorAll(".dot");
 
-const changeItem = (type) => {
-  const grid = document.getElementById("grid");
-  const gridContainer = document.getElementById("grid-container");
-  const gridContainerWidth = gridContainer.offsetWidth;
-  const previousBtn = document.getElementById("previous");
-  const nextBtn = document.getElementById("next");
-  const firstPageBtn = document.getElementById("first-page-btn");
-  const secondPageBtn = document.getElementById("second-page-btn");
-  const lastPageBtn = document.getElementById("last-page-btn");
-  const maxIndex =
-    gridContainerWidth === 1060 ? 3 : gridContainerWidth === 690 ? 4 : 5;
-  const itemsPerPage = gridContainerWidth === 1060 ? 3 : 2;
-  if (type === "previous" && index > 0) {
-    index--;
-    grid.style.transform = `translateX(${-index * 366}px)`;
-  } else if (type === "next" && index < maxIndex) {
-    index++;
-    grid.style.transform = `translateX(-${index * 366}px)`;
-  } else if (type === "first-page") {
-    index = 0;
-    grid.style.transform = "translateX(0px)";
-  } else if (type === "second-page") {
-    index = itemsPerPage;
-    grid.style.transform = `translateX(${-itemsPerPage * 366}px)`;
-  } else if (type === "last-page") {
-    index = 4;
-    grid.style.transform = `translateX(${-4 * 366}px)`;
+const handleChangeItem = (type, pageNumber) => {
+  const changeType = type;
+  if (changeType === "previous") {
+    if (index > 0) index--;
+    else index = maxIndex;
+  } else if (changeType === "next") {
+    if (index < maxIndex) index++;
+    else index = 0;
+  } else if (changeType === "page") {
+    index = pageNumber * itemsPerPage;
   }
-
-  if (index <= 0) {
-    previousBtn.classList.add("disable");
-  } else previousBtn.classList.remove("disable");
-
-  if (index >= maxIndex) {
-    nextBtn.classList.add("disable");
-  } else nextBtn.classList.remove("disable");
-
-  if (index >= 4) {
-    firstPageBtn.classList.remove("selected");
-    secondPageBtn.classList.remove("selected");
-    lastPageBtn.classList.add("selected");
-  } else if (index >= itemsPerPage) {
-    firstPageBtn.classList.remove("selected");
-    lastPageBtn.classList.remove("selected");
-    secondPageBtn.classList.add("selected");
-  } else {
-    secondPageBtn.classList.remove("selected");
-    lastPageBtn.classList.remove("selected");
-    firstPageBtn.classList.add("selected");
-  }
+  grid.style.transform = `translateX(${-index * 366}px)`;
+  dots.forEach((dot) => {
+    dot.classList.remove("selected");
+  });
+  dots[Math.round(index / itemsPerPage)].classList.add("selected");
 };
+
+document
+  .getElementById("previous")
+  .addEventListener("click", () => handleChangeItem("previous"));
+document
+  .getElementById("next")
+  .addEventListener("click", () => handleChangeItem("next"));
+document
+  .getElementById("page1")
+  .addEventListener("click", () => handleChangeItem("page", 0));
+document
+  .getElementById("page2")
+  .addEventListener("click", () => handleChangeItem("page", 1));
+document
+  .getElementById("page3")
+  .addEventListener("click", () => handleChangeItem("page", 2));
